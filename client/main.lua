1b21de86d043ac9f68c3fa2f13bbe29860b4baeb
Mikus Lorencs <mikuslorence@gmail.com>
2013-08-03 17:47:49 -0600

    libs n stuff
    - lube, hump, loveMoon, everythang in thar

---
+++ client/main.lua
@@ -0,0 +1,40 @@
+--Libraries
+require("libs/lovelyMoon/stateManager")
+require("libs/lovelyMoon/lovelyMoon")
+
+--GameStates
+require("states/gameState")
+require("states/startState")
+
+function love.load()
+	--Add Gamestates Here
+	addState(MenuState, "start")
+	addState(GameState, "game")
+	
+	--Remember to Enable your Gamestates!
+	enableState("start")
+end
+
+function love.update(dt)
+	lovelyMoon.update(dt)
+end
+
+function love.draw()
+	lovelyMoon.draw()
+end
+
+function love.keypressed(key, unicode)
+	lovelyMoon.keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+	lovelyMoon.keyreleased(key, unicode)
+end
+
+function love.mousepressed(x, y, button)
+	lovelyMoon.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+	lovelyMoon.mousereleased(x, y, button)
+end
---
+++ client/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cwd = %cd%
+start "" "%~dp0\dist\love.exe" "%~dp0\"
